30f08f6381bbe2f09de7275a111b5961a05cc850
Gaurav <59747089+GauravPoosarla@users.noreply.github.com>
2022-01-23 15:11:58 +0530

    Time: 7 ms (11.97%), Space: 8.5 MB (60.96%) - LeetHub

--- 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.cpp
+++ 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
-    void move(vector<int>& result, int index)
-    {
-        for(int i=result.size()-1; i>=index+1; i--)
-        {
-            result[i] = result[i-1];
-        }
-    }
     vector<int> createTargetArray(vector<int>& nums, vector<int>& index) {
         int n = nums.size();
-        vector<int> result(n, -1);
         
         for(int i=0; i<n; i++)
         {
-            if(result[index[i]] == -1)
+            for(int j=0; j<i; j++)
             {
-                result[index[i]] = nums[i];
-            }
-            else
-            {
-                move(result, index[i]);
-                result[index[i]] = nums[i];
+                if(index[j] >= index[i])
+                {
+                    index[j]++;
+                }
             }
         }
-        return result;
+        
+        vector<int> ans(n);
+        for(int i=0; i<n; i++)
+        {
+            ans[index[i]] = nums[i];
+        }
+        return ans;
     }
 };
