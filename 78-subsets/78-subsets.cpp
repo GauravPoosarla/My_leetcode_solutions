ddd5643af48a046107749d1b25a3122c5cdfe477
Gaurav <59747089+GauravPoosarla@users.noreply.github.com>
2022-01-26 18:45:11 +0530

    Time: 7 ms (21.33%), Space: 15 MB (25.90%) - LeetHub

---
+++ 78-subsets/78-subsets.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void subsets(vector<int>& nums, vector<int>& ds, vector<vector<int>>& result, int index)
+    {
+        if(index == nums.size())
+        {
+            vector<int> temp;
+            for(auto s: ds)
+            {
+                temp.push_back(s);
+            }
+            result.push_back(temp);
+            return;
+        }
+        ds.push_back(nums[index]);
+        subsets(nums, ds, result, index+1);
+        ds.pop_back();
+        subsets(nums, ds, result, index+1);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> result;
+        vector<int> ds;
+        subsets(nums, ds, result, 0);
+        return result;
+    }
+};
