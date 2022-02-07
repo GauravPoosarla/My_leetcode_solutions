2923fa3281d8d8a4a7245583abdce80ca583f495
Gaurav <59747089+GauravPoosarla@users.noreply.github.com>
2022-02-07 23:40:55 +0530

    Time: 17 ms (20.81%), Space: 5.8 MB (96.30%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,25 +1,36 @@
 class Solution {
 public:
     int fib(int n) {
-        // BruteForce
+        // BruteForce Iterative
         // TC: O(n)
         // SC: O(1)
         
-        int first = 0;
-        int second = 1;
+//         int first = 0;
+//         int second = 1;
+        
+//         if(n<2)
+//         {
+//             return n;
+//         }
+        
+//         int sum=0;
+//         for(int i=2; i<=n; i++)
+//         {
+//             sum = first+second;
+//             first = second;
+//             second = sum;
+//         }
+//         return second;
+        
+        // BruteForce Recursive
         
         if(n<2)
         {
             return n;
         }
         
-        int sum=0;
-        for(int i=2; i<=n; i++)
-        {
-            sum = first+second;
-            first = second;
-            second = sum;
-        }
-        return second;
+        int first = fib(n-1);
+        int second = fib(n-2);
+        return first + second;
     }
 };
