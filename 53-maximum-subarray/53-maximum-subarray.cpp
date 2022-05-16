541da13f3ff5cd8f1132a984ec21293ad9dd22b9
Gaurav <59747089+GauravPoosarla@users.noreply.github.com>
2022-05-16 22:18:13 +0530

    Time: 251 ms (5.14%), Space: 67.8 MB (55.16%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int max_so_far = INT_MIN;
+        int max_end_here = 0;
+        int start = 0;
+        int end = 0;
+        
+        for(int i=0; i<nums.size(); i++)
+        {
+            max_end_here += nums[i];
+            if(max_end_here > max_so_far)
+            {
+                max_so_far = max_end_here;
+                end = i;
+            }
+            if(max_end_here < 0)
+            {
+                max_end_here = 0;
+                start = i+1;
+            }
+        }
+        // cout << start << " " << end << endl;
+        return max_so_far;
+    }
+};
