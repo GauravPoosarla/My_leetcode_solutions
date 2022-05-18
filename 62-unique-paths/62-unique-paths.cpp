a4b2b871b8554ce9581a8ef76e671c1b5f8b4b89
Gaurav <59747089+GauravPoosarla@users.noreply.github.com>
2022-05-18 19:55:31 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (44.83%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,20 +1,26 @@
 class Solution {
-public:
-    int countPaths(int i,int j,int n,int m,vector<vector<int>> &dp)
+private:
+    int uniquePathsUtil(int i, int j, int m, int n, vector<vector<int>> &dp)
     {
-        if(i==(n-1)&&j==(m-1)) return 1;
-        if(i>=n||j>=m) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-     else return dp[i][j]=countPaths(i+1,j,n,m,dp)+countPaths(i,j+1,n,m,dp);
-        
+        if(i == m-1 and j == n-1)
+        {
+            return 1;
+        }
+        if(i >= m or j >= n)
+        {
+            return 0;
+        }
+        if(dp[i][j] != -1)
+        {
+            return dp[i][j];
+        }
+        else 
+            return dp[i][j] = uniquePathsUtil(i+1, j, m, n, dp) + uniquePathsUtil(i, j+1, m, n, dp);
     }
+public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
-       
-        //dp[1][1]=1;
-       int num=countPaths(0,0,m,n,dp);
-        if(m==1&&n==1)
-            return num;
-        return dp[0][0];
+        vector<vector<int>> dp(m+1, vector<int> (n+1, -1));
+        int num = uniquePathsUtil(0, 0, m, n, dp);
+        return num;
     }
 };
