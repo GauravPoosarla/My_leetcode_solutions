ac0d86b11febc2b19db25b87f5d244c437b696e7
Gaurav <59747089+GauravPoosarla@users.noreply.github.com>
2022-05-18 15:49:25 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (81.16%) - LeetHub

---
+++ 50-powx-n/50-powx-n.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans = 1.0;
+        long long nn = n; // for overflow
+        
+        if(nn < 0)
+        {
+            nn = -1 * nn;
+        }
+        while(nn)
+        {
+            if(nn % 2 == 0)
+            {
+                x *= x;
+                nn /= 2;
+            }
+            else
+            {
+                ans *= x;
+                nn -= 1;
+            }
+        }
+        if(n < 0)
+        {
+            return 1.0/ans;
+        }
+        return ans;
+    }
+};
