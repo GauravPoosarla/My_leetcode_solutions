c6a1f6254919733be2c4b1a4d24263fdb1935adf
Gaurav <59747089+GauravPoosarla@users.noreply.github.com>
2022-05-19 22:12:19 +0530

    Added solution - LeetHub

---
+++ 23. Longest Subarray Of Evens And Odds - GFG/23.-longest-subarray-of-evens-and-odds.cpp
@@ -0,0 +1,55 @@
+// { Driver Code Starts
+//Initial Template for C++
+#include<iostream> 
+using namespace std; 
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    //Complete this function
+    //Function to find the length of longest subarray of even and odd numbers.
+    int maxEvenOdd(int arr[], int n) 
+    { 
+       //Your code here
+       int count = 1, maxLen = 1;
+       for(int i=0; i<n-1; i++)
+       {
+           if(arr[i]%2 != arr[i+1]%2)
+           {
+               count++;
+               maxLen = max(maxLen, count);
+           }
+           else
+                count = 1;
+       }
+       return maxLen;
+    } 
+};
+
+// { Driver Code Starts.
+int main() 
+{ 
+    int testcases;
+    //testcases
+    cin>>testcases;
+    while(testcases--)
+    {
+        int sizeOfArray;
+        //size of array
+        cin>>sizeOfArray;
+        int arr[sizeOfArray];
+        
+        //inserting elements in the array
+        for(int i=0;i<sizeOfArray;i++)
+        {
+            cin>>arr[i];
+        }
+        Solution ob;
+        //calling function
+        cout  << ob.maxEvenOdd(arr, sizeOfArray)<<endl; 
+    }
+   return 0; 
+} 
+  // } Driver Code Ends
