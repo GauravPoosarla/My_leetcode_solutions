b9a397fdea0ce836ab7229f755bb0c06731e7941
Gaurav <59747089+GauravPoosarla@users.noreply.github.com>
2022-05-20 15:17:53 +0530

    Time: 11 ms (32.79%), Space: 8.6 MB (10.86%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.cpp
+++ 206-reverse-linked-list/206-reverse-linked-list.cpp
@@ -15,19 +15,9 @@ public:
         {
             return head;
         }
-        ListNode* prev_p = NULL;
-        ListNode* curr_p = head;
-        ListNode* next_p;
-        
-        while(curr_p != NULL)
-        {
-            next_p = curr_p->next;
-            curr_p->next = prev_p;
-            
-            prev_p = curr_p;
-            curr_p = next_p;
-        }
-        head = prev_p;
-        return head;
+        ListNode* temp = reverseList(head->next);
+        head->next->next = head;
+        head->next = NULL;
+        return temp;
     }
 };
