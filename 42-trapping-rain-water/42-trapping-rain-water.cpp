f2f7b031ce375307c3b9758ae173349a34cc0d83
Gaurav <59747089+GauravPoosarla@users.noreply.github.com>
2022-05-22 21:18:47 +0530

    Time: 24 ms (25.17%), Space: 19.8 MB (5.72%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -2,28 +2,31 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n = height.size();
-        int prefix[n];
-        int suffix[n];
+        int left = 0, right = n-1;
+        int res = 0;
+        int maxLeft = INT_MIN, maxRight = INT_MIN;
         
-        prefix[0] = height[0];
-        for(int i=1; i<n; i++)
+        while(left <= right)
         {
-            int val = max(prefix[i-1], height[i]);
-            prefix[i] = val;
+            if(height[left] <= height[right])
+            {
+                if(height[left] > maxLeft)
+                    maxLeft = height[left];
+                else
+                    res += maxLeft-height[left];
+                
+                left++;
+            }
+            else
+            {
+                if(height[right] >= maxRight)
+                    maxRight = max(maxRight, height[right]);
+                else
+                    res += maxRight - height[right];
+                
+                right--;
+            }
         }
-        
-        suffix[n-1] = height[n-1];
-        for(int i=n-2; i>=0; i--)
-        {
-            int val = max(suffix[i+1], height[i]);
-            suffix[i] = val;
-        }
-        
-        int waterTrapped = 0;
-        for(int i=0; i<n; i++)
-        {
-            waterTrapped += min(prefix[i], suffix[i]) - height[i];
-        }
-        return waterTrapped;
+        return res;
     }
 };
