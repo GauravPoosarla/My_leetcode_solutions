f1c220b9b0423f921afa85654746d4761dc97953
Gaurav <59747089+GauravPoosarla@users.noreply.github.com>
2022-05-23 21:35:07 +0530

    Added solution - LeetHub

---
+++ Fractional Knapsack - GFG/fractional-knapsack.cpp
@@ -0,0 +1,82 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Item{
+    int value;
+    int weight;
+};
+
+
+ // } Driver Code Ends
+//class implemented
+/*
+struct Item{
+    int value;
+    int weight;
+};
+*/
+
+
+class Solution
+{
+    public:
+    static bool comparator(Item a, Item b)
+    {
+        double r1 = (double)a.value/a.weight;
+        double r2 = (double)b.value/b.weight;
+        return r1 > r2;
+    }
+    //Function to get the maximum total value in the knapsack.
+    double fractionalKnapsack(int W, Item arr[], int n)
+    {
+        // Your code here
+        sort(arr, arr+n, comparator);
+        
+        int currWeight = 0;
+        double profit = 0.0;
+        
+        for(int i=0; i<n; i++)
+        {
+            if(currWeight + arr[i].weight <= W)
+            {
+                currWeight += arr[i].weight;
+                profit += arr[i].value;
+            }
+            else
+            {
+                double remainingWeight = W-currWeight;
+                profit += (double)remainingWeight * ((double) arr[i].value/arr[i].weight);
+                break;
+            }
+        }
+        return profit;
+    }
+        
+};
+
+
+// { Driver Code Starts.
+int main()
+{
+	int t;
+	//taking testcases
+	cin>>t;
+	cout<<setprecision(2)<<fixed;
+	while(t--){
+	    //size of array and weight
+		int n, W;
+		cin>>n>>W;
+		
+		Item arr[n];
+		//value and weight of each item
+		for(int i=0;i<n;i++){
+			cin>>arr[i].value>>arr[i].weight;
+		}
+		
+		//function call
+		Solution ob;
+		cout<<ob.fractionalKnapsack(W, arr, n)<<endl;
+	}
+    return 0;
+}  // } Driver Code Ends
