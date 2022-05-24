d71c0240220c773ae78ca40b0701d1bd44214095
Gaurav <59747089+GauravPoosarla@users.noreply.github.com>
2022-05-24 19:44:24 +0530

    Time: 0 ms (100.00%), Space: 10.5 MB (87.67%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.cpp
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 private:
-    void combinationSum2(vector<int>& candidates, int target, int index, vector<int> ds, vector<vector<int>>& result)
+    void combinationSum2(vector<int>& candidates, int target, vector<vector<int>>& result, vector<int>& ds, int index)
     {
         if(target == 0)
         {
@@ -9,12 +9,12 @@ private:
         }
         for(int i=index; i<candidates.size(); i++)
         {
-            if(i > index and candidates[i] == candidates[i-1])
+            if(i != index and candidates[i] == candidates[i-1])
                 continue;
-            if(target < candidates[i])
+            if(candidates[i] > target)
                 break;
             ds.push_back(candidates[i]);
-            combinationSum2(candidates, target-candidates[i], i+1, ds, result);
+            combinationSum2(candidates, target-candidates[i], result, ds, i+1);
             ds.pop_back();
         }
     }
@@ -24,7 +24,8 @@ public:
         vector<vector<int>> result;
         vector<int> ds;
         int index = 0;
-        combinationSum2(candidates, target, index, ds, result);
+        
+        combinationSum2(candidates, target, result, ds, index);
         return result;
     }
 };
