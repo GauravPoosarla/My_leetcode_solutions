5447a22de13be4f24cd79c6f6b4064c39cb9a4bc
Gaurav <59747089+GauravPoosarla@users.noreply.github.com>
2022-05-26 08:17:17 +0530

    Time: 15 ms (28.44%), Space: 13.5 MB (98.29%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,63 +1,51 @@
 class Solution {
 public:
-    int firstOcc(vector<int>& nums, int target)
-    {
-        int start = 0;
-        int end = nums.size()-1;
-        int ans = -1;
-        while(start<=end)
-        {
-            int mid = start + (end-start)/2;
-            
-            if(nums[mid] == target)
-            {
-                ans = mid;
-                end = mid-1;
-            }
-            else if(nums[mid] < target)
-            {
-                start = mid+1;
-            }
-            else
-            {
-                end = mid-1;
-            }
-        }
-        return ans;
-    }
-    
-    int lastOcc(vector<int>& nums, int target)
-    {
-        int start = 0;
-        int end = nums.size()-1;
-        int ans = -1;
-        while(start<=end)
-        {
-            int mid = start + (end-start)/2;
-            
-            if(nums[mid] == target)
-            {
-                ans = mid;
-                start = mid+1;
-            }
-            else if(nums[mid] < target)
-            {
-                start = mid+1;
-            }
-            else
-            {
-                end = mid-1;
-            }
-        }
-        return ans;
-    }
-    
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> ans;
-        int ans1 = firstOcc(nums, target);
-        int ans2 = lastOcc(nums, target);
-        ans.push_back(ans1);
-        ans.push_back(ans2);
-        return ans;
+        vector<int> result;
+        int start = 0;
+        int end = nums.size()-1;
+        int leftIndex = -1, rightIndex = -1;
+        
+        while(start <= end)
+        {
+            int mid = (start + end)/2;
+            if(nums[mid] == target)
+            {
+                leftIndex = mid;
+                end = mid-1;
+            }
+            else if(nums[mid] < target)
+            {
+                start = mid+1;
+            }
+            else
+            {
+                end = mid-1;
+            }
+        }
+        result.push_back(leftIndex);
+        
+        start = 0;
+        end = nums.size()-1;
+        
+        while(start <= end)
+        {
+            int mid = (start + end)/2;
+            if(nums[mid] == target)
+            {
+                rightIndex = mid;
+                start = mid+1;
+            }
+            else if(nums[mid] < target)
+            {
+                start = mid+1;
+            }
+            else
+            {
+                end = mid-1;
+            }
+        }
+        result.push_back(rightIndex);
+        return result;
     }
 };
