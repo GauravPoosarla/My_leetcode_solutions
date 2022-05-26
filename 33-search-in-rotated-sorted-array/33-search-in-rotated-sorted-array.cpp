8df121f045b734d058d8a63ab0d81d2ecfd1ce3d
Gaurav <59747089+GauravPoosarla@users.noreply.github.com>
2022-05-26 23:43:46 +0530

    Time: 3 ms (85.43%), Space: 11.1 MB (30.14%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int l = 0, r = nums.size()-1;
-        while (l <= r) {
-            int mid = (l+r) / 2;
-            if (target == nums[mid])
+    int search(vector<int>& a, int target) {
+        int low = 0, high = a.size()-1;
+        while(low <= high)
+        {
+            int mid = (low + high) / 2;
+            if(a[mid] == target)
                 return mid;
-            // there exists rotation; the middle element is in the left part of the array
-            if (nums[mid] > nums[r]) {
-                if (target < nums[mid] && target >= nums[l])
-                    r = mid - 1;
+            
+            // left side is sorted
+            if(a[low] <= a[mid])
+            {
+                if(target >= a[low] and target <= a[mid])
+                    high = mid - 1;
                 else
-                    l = mid + 1;
+                    low = mid + 1;
             }
-            // there exists rotation; the middle element is in the right part of the array
-            else if (nums[mid] < nums[l]) {
-                if (target > nums[mid] && target <= nums[r])
-                    l = mid + 1;
+            else
+            {
+                if(target >= a[mid] and target <= a[high])
+                    low = mid + 1;
                 else
-                    r = mid - 1;
-            }
-            // there is no rotation; just like normal binary search
-            else {
-                if (target < nums[mid])
-                    r = mid - 1;
-                else
-                    l = mid + 1;
+                    high = mid - 1;
             }
         }
         return -1;
