8a293db4acdea6f3ee4a9ae013a459e072ec51ca
Gaurav <59747089+GauravPoosarla@users.noreply.github.com>
2022-05-28 21:48:22 +0530

    Time: 540 ms (50.00%), Space: 112.6 MB (100.00%) - LeetHub

--- 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.cpp
+++ 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
     long long maximumImportance(int n, vector<vector<int>>& roads) {
-        
-        // indgree contains {indegree, node} of every node from 0 to n-1.
-        vector<pair<int, int>> indegree;
-        for (int i=0; i<n; i++) indegree.push_back({0, i});
-        for (auto rd : roads) {
-            indegree[rd[0]].first++;
-            indegree[rd[1]].first++;
+        vector<int> A(n,0);			//For counting the number of edges the given node is part of
+        for(int i=0;i<roads.size();i++)
+        {
+            A[roads[i][0]]++;
+            A[roads[i][1]]++;
         }
-                
-        sort(begin(indegree), end(indegree), greater<pair<int, int>>());
-        
-        int impt(n);
-        // importance contains importance assigned to all the nodes.
-        vector<int> importance(n, 0);
-        for (auto& b : indegree)  importance[b.second] = impt--;
-        
-        long long ans(0);
-        for (auto& road : roads)
-            ans += (importance[road[0]] + importance[road[1]]);
-        
+        sort(A.begin(),A.end());
+        long long ans=0;
+        for(int i=0;i<n;i++)
+        {
+            ans+=((long long)A[i]*(i+1));
+        }
         return ans;
     }
 };
