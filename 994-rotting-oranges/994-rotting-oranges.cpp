7a8e110f50fd9d8c2299c78d271fe9b7c044cefc
Gaurav <59747089+GauravPoosarla@users.noreply.github.com>
2022-05-31 02:16:37 +0530

    Time: 5 ms (83.83%), Space: 13.2 MB (49.92%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -1,35 +1,46 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        if(grid.empty()) return 0;
-        int m = grid.size(), n = grid[0].size(), days = 0, tot = 0, cnt = 0;
+        if(grid.empty())
+            return 0;
+        int m = grid.size(), n = grid[0].size(), minutes = 0, total = 0, count = 0;
+        
         queue<pair<int, int>> rotten;
-        for(int i = 0; i < m; ++i){
-            for(int j = 0; j < n; ++j){
-                if(grid[i][j] != 0) tot++;
-                if(grid[i][j] == 2) rotten.push({i, j});
+        for(int i=0; i<m; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                if(grid[i][j] != 0)
+                    total++;
+                if(grid[i][j] == 2)
+                    rotten.push({i, j});
             }
         }
         
         int dx[4] = {0, 0, 1, -1};
         int dy[4] = {1, -1, 0, 0};
         
-        while(!rotten.empty()){
+        while(!rotten.empty())
+        {
             int k = rotten.size();
-            cnt += k; 
-            while(k--){
-                int x = rotten.front().first, y = rotten.front().second;
+            count += k;
+            while(k--)
+            {
+                int x = rotten.front().first;
+                int y = rotten.front().second;
                 rotten.pop();
-                for(int i = 0; i < 4; ++i){
+                for(int i=0; i<4; i++)
+                {
                     int nx = x + dx[i], ny = y + dy[i];
-                    if(nx < 0 || ny < 0 || nx >= m || ny >= n || grid[nx][ny] != 1) continue;
+                    if(nx < 0 || ny < 0 || nx >= m || ny >= n || grid[nx][ny] != 1)
+                        continue;
                     grid[nx][ny] = 2;
                     rotten.push({nx, ny});
                 }
             }
-            if(!rotten.empty()) days++;
+            if(!rotten.empty())
+                minutes++;
         }
-        
-        return tot == cnt ? days : -1;
+        return total == count ? minutes : -1;
     }
 };
