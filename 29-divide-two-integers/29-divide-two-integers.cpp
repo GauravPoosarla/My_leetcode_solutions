4d257043fdc9387c11715a9d2aa6a8e5652e81c9
Gaurav <59747089+GauravPoosarla@users.noreply.github.com>
2022-05-30 22:02:35 +0530

    Time: 7 ms (27.04%), Space: 6 MB (30.45%) - LeetHub

--- 29-divide-two-integers/29-divide-two-integers.cpp
+++ 29-divide-two-integers/29-divide-two-integers.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
-    int divide(int A, int B) {
-        // for the corner case, If A is INT_MIN, and B is -1, 
-        // the result will be 1 + INT_MAX, hence overflowing 
-        // 32 bits, handle it first
-        if (A == INT_MIN && B == -1) 
+    int divide(int dividend, int divisor) {
+        if (dividend == INT_MIN && divisor == -1) {
             return INT_MAX;
-        
-        // let's handle sign at the end, keep unsigned values for shifts
-        unsigned int a = abs(A);
-        unsigned int b = abs(B);
-        unsigned int res = 0;
-        
-        // iterate over each possible factors from 1 to 2^31
-        for (int x = 31; x >= 0; x--) {
-            // Is it possible to reduce "a" up to a >> x
-            // such that a >> x is still grater than b ?
-            if ((a >> x) >= b) {
-                a = a - (b << x); // adjust the dividend, substract the maximum possible multiple of b and 1 << x
-                res = res + (1 << x); //update the quotient
-            }
         }
-        return (A > 0) == (B > 0) ? res : -res; //usual stuff, nothing fancy
+        long dvd = labs(dividend), dvs = labs(divisor), ans = 0;
+        int sign = dividend > 0 ^ divisor > 0 ? -1 : 1;
+        while (dvd >= dvs) {
+            long temp = dvs, m = 1;
+            while (temp << 1 <= dvd) {
+                temp <<= 1;
+                m <<= 1;
+            }
+            dvd -= temp;
+            ans += m;
+        }
+        return sign * ans;
     }
 };
