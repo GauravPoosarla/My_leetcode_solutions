5dcf445fd3dd95dafbecb1817b36054cbb601ec9
Gaurav <59747089+GauravPoosarla@users.noreply.github.com>
2022-06-03 02:17:56 +0530

    Time: 4 ms (38.33%), Space: 8.4 MB (64.43%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
@@ -10,32 +10,39 @@
  * };
  */
 class Solution {
+private:
+    vector < int > postOrderTrav(TreeNode * cur) {
+
+  vector < int > postOrder;
+  if (cur == NULL) return postOrder;
+
+  stack < TreeNode * > st;
+  while (cur != NULL || !st.empty()) {
+
+    if (cur != NULL) {
+      st.push(cur);
+      cur = cur -> left;
+    } else {
+      TreeNode * temp = st.top() -> right;
+      if (temp == NULL) {
+        temp = st.top();
+        st.pop();
+        postOrder.push_back(temp -> val);
+        while (!st.empty() && temp == st.top() -> right) {
+          temp = st.top();
+          st.pop();
+          postOrder.push_back(temp -> val);
+        }
+      } else cur = temp;
+    }
+  }
+  return postOrder;
+
+}
 public:
     vector<int> postorderTraversal(TreeNode* root) {
-        vector<int> postorder;
-        if(root == NULL)
-            return postorder;
-        stack<TreeNode*> st1, st2;
-        st1.push(root);
-        
-        while(!st1.empty())
-        {
-            root = st1.top();
-            st1.pop();
-            
-            st2.push(root);
-            
-            if(root->left != NULL)
-                st1.push(root->left);
-            if(root->right != NULL)
-                st1.push(root->right);
-        }
-        
-        while(!st2.empty())
-        {
-            postorder.push_back(st2.top()->val);
-            st2.pop();
-        }
-        return postorder;
+        vector<int> result;
+        result = postOrderTrav(root);
+        return result;
     }
 };
