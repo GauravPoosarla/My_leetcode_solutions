85e56d0ddb22ff95c5ee1f71cb26d12e0de59a5e
Gaurav <59747089+GauravPoosarla@users.noreply.github.com>
2022-06-14 21:58:17 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (25.60%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,26 +1,23 @@
 class Solution {
 private:
-    int uniquePathsUtil(int i, int j, int m, int n, vector<vector<int>> &dp)
+    int uniquePath(int i, int j, int m, int n, vector<vector<int>>& dp)
     {
-        if(i == m-1 and j == n-1)
-        {
+        if(i == 0 and j == 0)
             return 1;
-        }
-        if(i >= m or j >= n)
-        {
+        if(i < 0 or j < 0)
             return 0;
-        }
         if(dp[i][j] != -1)
-        {
             return dp[i][j];
-        }
-        else 
-            return dp[i][j] = uniquePathsUtil(i+1, j, m, n, dp) + uniquePathsUtil(i, j+1, m, n, dp);
+        
+        int left = uniquePath(i, j-1, m, n, dp);
+        int right = uniquePath(i-1, j, m, n, dp);
+        
+        return dp[i][j] = left + right;
     }
+        
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m+1, vector<int> (n+1, -1));
-        int num = uniquePathsUtil(0, 0, m, n, dp);
-        return num;
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return uniquePath(m-1, n-1, m, n, dp);
     }
 };
