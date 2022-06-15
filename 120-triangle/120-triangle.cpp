e29a971a01b49d8f7841a0d6d8e9f95c13661ca8
Gaurav <59747089+GauravPoosarla@users.noreply.github.com>
2022-06-15 15:44:35 +0530

    Time: 8 ms (62.59%), Space: 8.7 MB (47.83%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -4,23 +4,24 @@ public:
         int m = triangle.size();
         int n = triangle[triangle.size()-1].size();
         
-        vector<vector<int>> dp(n, vector<int>(n, 0));
+        vector<int> nextRow(m, 0), currRow(m, 0);
         
         for(int i=0; i<m; i++)
         {
-            dp[m-1][i] = triangle[m-1][i];
+            nextRow[i] = triangle[m-1][i];
         }
         
         for(int i = m-2; i >= 0; i--)
         {
             for(int j = i; j >= 0; j--)
             {
-                int down = triangle[i][j] + dp[i+1][j];
-                int diag_down = triangle[i][j] + dp[i+1][j+1];
+                int down = triangle[i][j] + nextRow[j];
+                int diag_down = triangle[i][j] + nextRow[j+1];
                 
-                dp[i][j] = min(down, diag_down);
+                currRow[j] = min(down, diag_down);
             }
+            nextRow = currRow;
         }
-        return dp[0][0];
+        return nextRow[0];
     }
 };
