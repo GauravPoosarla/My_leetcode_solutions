55e684f9fd435a99706b57a7110edd4c23fe835b
Gaurav <59747089+GauravPoosarla@users.noreply.github.com>
2022-06-15 20:58:39 +0530

    Time: 107 ms (54.04%), Space: 14.4 MB (48.97%) - LeetHub

---
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -0,0 +1,50 @@
+class Solution {
+private:
+    int cherryPickup(int i1, int j1, int j2, int rows, int cols, vector<vector<int>>& grid, vector<vector<vector<int>>>& dp)
+    {
+        if(j1 < 0 or j1 >= cols or j2 < 0 or j2 >= cols) // out of bound conditions
+            return -1e8;
+        
+        if(dp[i1][j1][j2] != -1)
+            return dp[i1][j1][j2];
+        
+        if(i1 == rows-1) // reached destination
+        {
+            if(j1 == j2) // both robots are on the same cell. So, we have to consider only once.
+                return grid[i1][j1];
+            else
+                return grid[i1][j1] + grid[i1][j2]; // both are on different cells. So, we can collect on both.
+        }
+        
+        // possible actions:- 3*3 actions possible because we are moving both the robots simultaneously.
+        
+        
+        int maxi = -1e8; // maintaining a maximum variable
+        for(int dj1 = -1; dj1 <= +1; dj1++)
+        {
+            for(int dj2 = -1; dj2 <= +1; dj2++)
+            {
+                int value = 0;
+                if(j1 == j2)
+                    value = grid[i1][j1];
+                else
+                    value = grid[i1][j1] + grid[i1][j2];
+                
+                value += cherryPickup(i1+1, j1+dj1, j2+dj2, rows, cols, grid, dp);
+                maxi = max(maxi, value);
+            }
+        }
+        return dp[i1][j1][j2] = maxi;
+    }
+public:
+    int cherryPickup(vector<vector<int>>& grid) {
+        int i1 = 0, j1 = 0; // represents robot-1
+        int i2 = 0, j2 = grid[0].size() - 1; // represents robot-2
+        
+        int m = grid.size(); // total number of rows
+        int n = grid[0].size(); // total number of columns
+        
+        vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(n, -1)));
+        return cherryPickup(i1, j1, j2, m, n, grid, dp); // helper recursive function
+    }
+};
