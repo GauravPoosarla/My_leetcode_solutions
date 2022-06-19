3f29c077c8288d7f9097ce68d1f7b8af6f9e9fed
Gaurav <59747089+GauravPoosarla@users.noreply.github.com>
2022-06-19 18:43:42 +0530

    Time: 22 ms (37.30%), Space: 10.2 MB (33.81%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.cpp
+++ 130-surrounded-regions/130-surrounded-regions.cpp
@@ -1,43 +1,64 @@
 class Solution {
 private:
-    void dfs(int i, int j, vector<vector<char>>& board)
+    bool isValid(int i, int j, int rows, int cols)
     {
-        if(i < 0 or i >= board.size() or j < 0 or j >= board[0].size())
-            return;
-        if(board[i][j] != 'O')
-            return;
-        
+        if(i < 0 or i >= rows or j < 0 or j >= cols)
+            return false;
+        return true;
+    }
+    void bfs(int i, int j, vector<vector<char>>& board)
+    {
+        int m = board.size();
+        int n = board[0].size();
         board[i][j] = 'Y';
+        
         int dx[4] = {0, 1, 0, -1};
         int dy[4] = {1, 0, -1, 0};
         
-        for(int k=0; k<4; k++)
+        queue<pair<int,int>> q;
+        q.push({i, j});
+        while(!q.empty())
         {
-            dfs(i+dx[k], j+dy[k], board);
+            int currx = q.front().first;
+            int curry = q.front().second;
+            q.pop();
+            
+            for(int i=0; i<4; i++)
+            {
+                int nx = currx + dx[i];
+                int ny = curry + dy[i];
+                if(!isValid(nx, ny, m, n))
+                    continue;
+                if(board[nx][ny] == 'O')
+                {
+                    q.push({nx, ny});
+                    board[nx][ny] = 'Y';
+                }
+            }
         }
-        return;
     }
 public:
     void solve(vector<vector<char>>& board) {
-        if(board.size() == 0 or board[0].size() == 0)
-            return;
         int rows = board.size();
         int cols = board[0].size();
+        
+        if(rows == 0 or cols == 0)
+            return;
         
         for(int i=0; i<cols; i++)
         {
             if(board[0][i] == 'O')
-                dfs(0, i, board);
+                bfs(0, i, board);
             if(board[rows-1][i] == 'O')
-                dfs(rows-1, i, board);
+                bfs(rows-1, i, board);
         }
         
-        for(int i=1; i<rows-1; i++)
+        for(int i=0; i<rows; i++)
         {
             if(board[i][0] == 'O')
-                dfs(i, 0, board);
+                bfs(i, 0, board);
             if(board[i][cols-1] == 'O')
-                dfs(i, cols-1, board);
+                bfs(i, cols-1, board);
         }
         
         for(int i=0; i<rows; i++)
