46cbd8e030b43c44c89e722e53e357882157b299
Gaurav <59747089+GauravPoosarla@users.noreply.github.com>
2022-06-19 22:08:15 +0530

    Time: 10 ms (98.16%), Space: 10.2 MB (47.20%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -1,32 +1,41 @@
 class Solution {
-    int minFallingPathSum(int i, int j, int m, int n, vector<vector<int>>& matrix, vector<vector<int>>& dp)
-    {
-        if(j < 0 or j >= n)
-            return 1e8;
-        
-        if(i == 0)
-            return matrix[i][j];
-        
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        
-        int up = matrix[i][j] + minFallingPathSum(i-1, j, m, n, matrix, dp);
-        int up_left = matrix[i][j] + minFallingPathSum(i-1, j-1, m, n, matrix, dp);
-        int up_right = matrix[i][j] + minFallingPathSum(i-1, j+1, m, n, matrix, dp);
-        
-        return dp[i][j] = min({up, up_left, up_right});
-    }
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
+        int rows = matrix.size();
+        int cols = matrix[0].size();
         
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        int mini = INT_MAX;
-        for(int j=0; j<n; j++)
+        vector<vector<int>> dp(rows, vector<int>(cols, 0));
+        
+        for(int j = 0; j<cols; j++)
         {
-            int ans = minFallingPathSum(m-1, j, m, n, matrix, dp);
-            mini = min(mini, ans);
+            dp[0][j] = matrix[0][j];
+        }
+        
+        for(int i=1; i<rows; i++)
+        {
+            for(int j=0; j<cols; j++)
+            {
+                int up = matrix[i][j] + dp[i-1][j];
+                
+                int up_left = matrix[i][j];
+                if(j>0)
+                    up_left += dp[i-1][j-1];
+                else
+                    up_left += 1e9;
+                
+                int up_right = matrix[i][j];
+                if(j+1 < cols)
+                    up_right += dp[i-1][j+1];
+                else
+                    up_right += 1e9;
+                
+                dp[i][j] = min({up, up_left, up_right});
+            }
+        }
+        int mini = INT_MAX;
+        for(int i=0; i<cols; i++)
+        {
+            mini = min(mini, dp[rows-1][i]);
         }
         return mini;
     }
