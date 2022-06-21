a9bc1b390af24fb90706a3b0612cc1b61dc77d3b
Gaurav <59747089+GauravPoosarla@users.noreply.github.com>
2022-06-21 19:28:25 +0530

    Time: 24 ms (13.74%), Space: 10.5 MB (55.19%) - LeetHub

---
+++ 841-keys-and-rooms/841-keys-and-rooms.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+        bool canVisitAllRooms(vector<vector<int>>& rooms) 
+        {
+        stack<int> dfs; 
+        dfs.push(0);
+            
+        unordered_set<int> seen = {0};
+            
+        while (!dfs.empty()) 
+        {
+            int i = dfs.top(); 
+            dfs.pop();
+            for (int j : rooms[i])
+            {
+                if (seen.count(j) == 0) 
+                {
+                    dfs.push(j);
+                    seen.insert(j);
+                }
+            }
+        }
+        return rooms.size() == seen.size();
+    }
+};
