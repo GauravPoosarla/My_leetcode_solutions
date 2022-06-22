a108db05701069757e549fd3411fb407ffaa8d5e
Gaurav <59747089+GauravPoosarla@users.noreply.github.com>
2022-06-22 20:13:35 +0530

    Time: 5 ms (70.37%), Space: 9.6 MB (39.27%) - LeetHub

---
+++ 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.cpp
@@ -0,0 +1,26 @@
+class Solution {
+private:
+    bool checkOrder(string& a, string& b, unordered_map<char, int>& mp)
+    {
+        int i = -1;
+        while(++i < a.size() and i < b.size())
+        {
+            if(mp[a[i]] != mp[b[i]])
+                return mp[a[i]] < mp[b[i]];
+        }
+        return a.size() <= b.size();
+    }
+public:
+    bool isAlienSorted(vector<string>& words, string order) {
+        unordered_map<char, int> mp;
+        for(int i = 0; i< order.size(); i++)
+        {
+            mp[order[i]] = i;
+        }
+        
+        for(int i=0; i<words.size()-1; i++)
+            if(!checkOrder(words[i], words[i+1], mp))
+                return false;
+        return true;
+    }
+};
