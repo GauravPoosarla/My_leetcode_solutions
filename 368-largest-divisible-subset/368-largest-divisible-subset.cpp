746922b14fc3686ed44404170519de4b5daf961c
Gaurav <59747089+GauravPoosarla@users.noreply.github.com>
2022-06-23 02:15:14 +0530

    Time: 56 ms (87.22%), Space: 8.9 MB (76.28%) - LeetHub

--- 368-largest-divisible-subset/368-largest-divisible-subset.cpp
+++ 368-largest-divisible-subset/368-largest-divisible-subset.cpp
@@ -4,22 +4,27 @@ public:
         int i, prev, n = a.size(), ans = 1, lastIndex = 0, ind = 1;
         sort(a.begin(), a.end());
         vector<int> dp(n, 1), hash(n);
-        for(i = 0; i < n; i++) {
+        for(i = 0; i < n; i++) 
+        {
             hash[i] = i;
-            for(prev = 0; prev < i; prev++) {
-                if(a[i] % a[prev] == 0 && dp[i] < 1 + dp[prev]) {
+            for(prev = 0; prev < i; prev++) 
+            {
+                if(a[i] % a[prev] == 0 && dp[i] < 1 + dp[prev]) 
+                {
                     dp[i] = 1 + dp[prev];
                     hash[i] = prev;
                 }
             }
-            if(ans < dp[i]) {
+            if(ans < dp[i]) 
+            {
                 ans = dp[i];
                 lastIndex = i;
             }
         }
         vector<int> lis(ans);
         lis[0] = a[lastIndex];
-        while(hash[lastIndex] != lastIndex) {
+        while(hash[lastIndex] != lastIndex) 
+        {
             lastIndex = hash[lastIndex];
             lis[ind++] = a[lastIndex];
         }
