680f55eef646a064e85eac221de962bce9e9bf1e
Gaurav <59747089+GauravPoosarla@users.noreply.github.com>
2022-06-25 20:23:52 +0530

    Time: 4 ms (33.49%), Space: 5.9 MB (31.90%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.cpp
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -1,25 +1,15 @@
 class Solution {
-    bool f(int index, vector<int>& dp) {
-        
-        if(index <= 1)
-            return false;
-        
-        if(dp[index] != -1)
-            return dp[index];
-        
-        for(int x=1; x<index; x++)
-        {
-            if(index % x == 0)
-            {
-                if(f(index-x, dp) == false)
-                    return dp[index] = true;
-            }
-        }
-        return dp[index] = false;
-    }
 public:
     bool divisorGame(int n) {
-        vector<int> dp(n+1, -1);
-        return f(n, dp);
+        vector<bool> dp(n+1, false);
+        dp[0] = false;
+        dp[1] = false;
+        
+        for(int x = 2; x <= n; x++)
+        {
+            if(dp[x-1] == false)
+                dp[x] = true;
+        }
+        return dp[n];
     }
 };
