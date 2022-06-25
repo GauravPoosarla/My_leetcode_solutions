1c2e0263ac2734375326fb9146e44525cf7e07bf
Gaurav <59747089+GauravPoosarla@users.noreply.github.com>
2022-06-25 21:16:22 +0530

    Time: 8 ms (15.16%), Space: 6.3 MB (25.18%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -17,7 +17,20 @@ private:
     }
 public:
     int numDecodings(string s) {
-        vector<int> dp(s.size(), -1);
-        return f(0, s, dp);
+        int n = s.size();
+        vector<int> dp(n+1, 0);
+        dp[n] = 1;
+        for(int i=n-1; i>=0; i--)
+        {
+            if(s[i] == '0')
+                dp[i] = 0;
+            else
+            {
+                dp[i] = dp[i+1];
+                if(i < n-1 and (s[i] == '1' or (s[i] == '2' and s[i+1] < '7')))
+                    dp[i] += dp[i+2];
+            }
+        }
+        return dp[0];
     }
 };
