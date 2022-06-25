fd992989a61d268d9006d0bde723e889b615a1eb
Gaurav <59747089+GauravPoosarla@users.noreply.github.com>
2022-06-25 19:16:35 +0530

    Time: 5 ms (11.13%), Space: 6.1 MB (30.43%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,35 +1,20 @@
 class Solution {
-    int climbStairsUtil(int n, vector<int>& dp)
-    {        
-        if(n == 0)
-        {
-            return 1;
-        }
+private:
+    int f(int index, vector<int>& dp)
+    {
+        if(index <= 2)
+            return index;
+        if(dp[index] != -1)
+            return dp[index];
         
-        if(n == 1)
-        {
-            return 1;
-        }
+        int one = f(index-1, dp);
+        int two = f(index-2, dp);
         
-        if(dp[n] != -1)
-        {
-            return dp[n];
-        }
-        
-        
-        return dp[n] = climbStairs(n-1) + climbStairs(n-2);
+        return dp[index] = one + two;
     }
 public:
-    int climbStairs(int n) 
-    {
-          vector<int> dp(n+1, -1);
-  
-          dp[0]= 1;
-          dp[1]= 1;
-
-          for(int i=2; i<=n; i++){
-              dp[i] = dp[i-1]+ dp[i-2];
-          }
-          return dp[n];
+    int climbStairs(int n) {
+        vector<int> dp(n+1, -1);
+        return f(n, dp);
     }
 };
