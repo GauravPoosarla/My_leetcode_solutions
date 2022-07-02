fed0fc78daf92aea1ee8f561a3eed76ba6f705b9
Gaurav <59747089+GauravPoosarla@users.noreply.github.com>
2022-07-02 17:30:26 +0530

    Time: 57 ms (74.22%), Space: 31.5 MB (68.92%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,9 +1,23 @@
 class Solution {
 public:
-    bool canReach(vector<int>& A, int cur) {
-        if(cur < 0 || cur >= size(A) || A[cur] < 0) 
-            return false;                 
-        A[cur] *= -1;       // mark as visited by making -ve
-        return A[cur] == 0 || canReach(A, cur + A[cur]) || canReach(A, cur - A[cur]);  
+    bool canReach(vector<int>& arr, int start) {
+        queue<int> q;
+        q.push(start);
+        
+        while(!q.empty())
+        {
+            int curr = q.front();
+            q.pop();
+            if(arr[curr] == 0)
+                return true;
+            if(arr[curr] < 0)
+                continue;
+            if(curr + arr[curr] < arr.size())
+                q.push(curr + arr[curr]);
+            if(curr - arr[curr] >= 0)
+                q.push(curr - arr[curr]);
+            arr[curr] *= -1;
+        }
+        return false;
     }
 };
