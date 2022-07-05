caf28a32fa260c2baf780f53cccfcac5b508888f
Gaurav <59747089+GauravPoosarla@users.noreply.github.com>
2022-07-06 01:51:40 +0530

    Time: 22 ms (10.88%), Space: 18 MB (5.03%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,14 +1,33 @@
 class Solution {
+private:
+    long getAns(vector<int>& Arr, int ind, int buy, int n, vector<vector<long>> &dp )
+    {
+
+    if(ind==n) return 0; //base case
+    
+    if(dp[ind][buy]!=-1)
+        return dp[ind][buy];
+        
+    long profit;
+    
+    if(buy==0){// We can buy the stock
+        profit = max(0+getAns(Arr,ind+1,0,n,dp), -Arr[ind] + getAns(Arr,ind+1,1,n,dp));
+    }
+    
+    if(buy==1){// We can sell the stock
+        profit = max(0+getAns(Arr,ind+1,1,n,dp), Arr[ind] + getAns(Arr,ind+1,0,n,dp));
+    }
+    
+    return dp[ind][buy] = profit;
+}
 public:
-    int maxProfit(vector<int>& prices) {      
-        int profit=0,n = prices.size();
-        for(int i=0;i<n-1;++i)
-        {
-            if(prices[i+1]>prices[i])
-            {
-                profit += prices[i+1] - prices[i];
-            }
-        }
-        return profit;
+    int maxProfit(vector<int>& arr) 
+    {
+        int n = arr.size();
+        vector<vector<long>> dp(n,vector<long>(2,-1));
+    
+        if(n==0) return 0;
+        long ans = getAns(arr,0,0,n,dp);
+        return ans;
     }
 };
