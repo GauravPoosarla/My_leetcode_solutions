f5b141561f3e8c8426946ab4f3163c911600f76d
Gaurav <59747089+GauravPoosarla@users.noreply.github.com>
2022-07-07 01:46:56 +0530

    Time: 15 ms (61.15%), Space: 13.2 MB (25.03%) - LeetHub

---
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int maxProfit(int k, vector<int>& prices) {
+        return maximumProfit(prices, prices.size(), k);
+    }
+    int maximumProfit(vector<int>& Arr, int n, int k)
+{
+    // Creating a 3d - dp of size [n+1][2][k+1] initialized to 0
+    vector<vector<vector<int>>> dp(n+1,
+                                    vector<vector<int>> 
+                                            (2,vector<int>(k+1,0)));
+                                            
+    // As dp array is initialized to 0, we have already covered the base case
+    
+    for(int ind = n-1; ind>=0; ind--){
+        for(int buy = 0; buy<=1; buy++){
+            for(int cap=1; cap<=k; cap++){
+                
+                if(buy==0){// We can buy the stock
+                    dp[ind][buy][cap] = max(0+dp[ind+1][0][cap], 
+                                -Arr[ind] + dp[ind+1][1][cap]);
+                 }
+    
+                if(buy==1){// We can sell the stock
+                    dp[ind][buy][cap] = max(0+dp[ind+1][1][cap],
+                                Arr[ind] + dp[ind+1][0][cap-1]);
+                }
+            }
+        }
+    }
+    
+    
+    return dp[0][0][k];
+   
+}
+};
