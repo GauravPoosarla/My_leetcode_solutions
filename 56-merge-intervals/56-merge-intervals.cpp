de6e968084858c2c8d97b234ef11827d1ecae78a
Gaurav <59747089+GauravPoosarla@users.noreply.github.com>
2022-07-20 10:07:17 +0530

    Time: 48 ms (74.22%), Space: 19.1 MB (66.50%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -6,14 +6,10 @@ public:
         
         for(int i=0; i<intervals.size(); i++)
         {
-            if(merged.empty() || merged.back()[1] < intervals[i][0])
-            {
+            if(merged.empty() or merged.back()[1] < intervals[i][0])
                 merged.push_back({intervals[i][0], intervals[i][1]});
-            }
             else
-            {
                 merged.back()[1] = max(merged.back()[1], intervals[i][1]);
-            }
         }
         return merged;
     }
